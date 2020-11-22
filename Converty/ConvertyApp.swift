4cfbc1442ea6232917c314135a6896503e4ce87c
Ivan Rzhanoi <ivan.rzhanoi@ecolane.com>
2020-11-22 15:34:57 +0200

    Initial Commit

---
+++ Converty/ConvertyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConvertyApp.swift
+//  Converty
+//
+//  Created by Ivan Rzhanoi on 22.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct ConvertyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
